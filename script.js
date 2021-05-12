f5910fa4d40fbbeddf91a79d937b192117958047
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-05-12 03:05:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+const container = document.querySelector(".container");
+
+hamburgerMenu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
